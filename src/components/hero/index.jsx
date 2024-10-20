c9e7d7ac51dc5bac02ab360f292ef4c6d99fb66f
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-20 16:04:56 +0300

    hero updated

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import HeroCard from './HeroCard';
+import HeroBrand from './HeroBrand';
 
 const Hero = () => {
   return (
     <div className="mt-[30px] 2xl:mt-[110px] grid lg:grid-cols-2 gap-[50px]">
-      
+      <div>
+        <HeroBrand />
+      </div>
 
-     <div>
-      <HeroCard/>
-     </div>
+      <div>
+        <HeroCard />
+      </div>
     </div>
   );
 };
