59fcb24430e5c91a4f80b20dedc67dc201a86ccb
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-20 21:29:14 +0300

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Home from './pages/home';
+import Loader from './components/loader/Loader';
 
 const App = () => {
   return (
