ffec951ff66716636096726dc223b5b8dc791cdf
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-21 14:24:48 +0300

    SelectType updated

--- src/components/card/SelectType.jsx
+++ src/components/card/SelectType.jsx
@@ -6,7 +6,7 @@ const SelectType = ({ selectedType, setSelectedType }) => {
       <button
         onClick={() => setSelectedType('cornet')}
         className={`flex-1 border rounded-md py-2 px-6 text-center hover:bg-white/30 cursor-pointer transition ${
-          selectedType === 'cornet' && 'bg-white text-black'
+          selectedType === 'cornet' && 'bg-white text-black shadow-lg'
         }`}
       >
         Cone
@@ -14,7 +14,7 @@ const SelectType = ({ selectedType, setSelectedType }) => {
       <button
         onClick={() => setSelectedType('cup')}
         className={`flex-1 border rounded-md py-2 px-6 text-center hover:bg-white/30 cursor-pointer transition
-            ${selectedType === 'cup' && 'bg-white text-black'}`}
+            ${selectedType === 'cup' && 'bg-white text-black shadow-lg'}`}
       >
         Cup
       </button>
