81c55a47c7f3210acc28b650b70f2f6b5e2ab55c
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-22 15:24:42 +0300

    SelectType test added

---
+++ src/__test__/selecttype.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import SelectType from '../components/card/SelectType';
+import userEvent from '@testing-library/user-event';
+
+describe('Select Type Component', () => {
+  const mockFn = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('calls handleClick with the correct parameter when button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<SelectType selectedType={null} setSelectedType={mockFn} />);
+
+    const cornetBtn = screen.getByRole('button', { name: 'Cone' });
+    const cupBtn = screen.getByRole('button', { name: 'Cup' });
+
+    await user.click(cornetBtn);
+
+    expect(mockFn).toHaveBeenCalledWith('cornet');
+
+    await user.click(cupBtn);
+
+    expect(mockFn).toHaveBeenCalledWith('cup');
+  });
+
+
+});
