3ed8bad1dba8a09ce88d780b1c0d853c300a32ea
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-22 12:45:33 +0300

    package.json & list test updated

--- src/__test__/list.test.jsx
+++ src/__test__/list.test.jsx
@@ -1,20 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,waitFor } from '@testing-library/react';
 import List from '../components/list';
 import api from '../api';
 
 jest.mock('../api');
 
 describe('List component test', () => {
-  it('When fetching data, a loader is displayed on the screen', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('When fetching data, a loader is displayed on the screen', async () => {
     api.get.mockResolvedValueOnce({ data: [] });
 
     render(<List />);
 
     screen.getByTestId('loader');
-
+    await waitFor(() => {
+      expect(screen.queryByTestId('loader')).not.toBeInTheDocument();
+    });
   });
 
-  it("If an error response is received from the API, an error message is displayed on the screen", () => {});
+  it('If an error response is received from the API, an error message is displayed on the screen', async () => {
 
-  it("If a successful response is received from the API, cards are displayed on the screen", () => {});
+    const errMsg = 'Request timeout';
+
+    api.get.mockRejectedValueOnce(new Error(errMsg));
+
+    render(<List />);
+
+    await waitFor(() => screen.getByText(errMsg));
+  });
+
+  it('If a successful response is received from the API, cards are displayed on the screen', () => {});
 });
