bc826f2899e60365657f7e53c6f88bacba76eb95
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-27 15:14:15 +0300

    amount-picker test completed

--- src/__test__/amount-picker.test.jsx
+++ src/__test__/amount-picker.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import AmountPicker from '../components/card/AmountPicker';
 import { useDispatch } from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import { addToCart, deleteFromCart } from '../redux/cartSlice';
 
 jest.mock('react-redux', () => ({
   useDispatch: jest.fn(),
@@ -16,10 +18,30 @@ describe('Amount Picker', () => {
     quantity: 2,
   };
 
+  const dispatchMock = jest.fn();
+  beforeEach(() => {
+    useDispatch.mockReturnValue(dispatchMock);
+    dispatchMock.mockClear();
+  });
+
   it('quantity value is correct', () => {
     render(<AmountPicker item={item} />);
-    screen.getByText("2");
+    screen.getByText('2');
   });
-  it('Clicking the minus button triggers the correct action', () => {});
-  it('Clicking the plus button triggers the correct action', () => {});
+  it('- Clicking the minus button triggers the correct action', async () => {
+    const user = userEvent.setup();
+    render(<AmountPicker item={item} />);
+    const btn = screen.getByRole('button', { name: '-' });
+    await user.click(btn);
+    expect(dispatchMock).toHaveBeenCalledWith(deleteFromCart(item));
+  });
+  it('Clicking the plus button triggers the correct action', async () => {
+    const user = userEvent.setup();
+    render(<AmountPicker item={item} />);
+    const btn = screen.getByRole('button', { name: '+' });
+    await user.click(btn);
+    expect(dispatchMock).toHaveBeenCalledWith(
+      addToCart({ item, selectedType: item.selectedType })
+    );
+  });
 });
